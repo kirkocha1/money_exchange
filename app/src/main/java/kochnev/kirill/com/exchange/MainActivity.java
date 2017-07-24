6d54b1cb6fa2d050dcdb3e55868c2bf3d4114ca6
Kirill Kochnev <kochnev@sibext.com>
2017-07-25 00:31:13 +0700

    init project

---
+++ app/src/main/java/kochnev/kirill/com/exchange/MainActivity.java
@@ -0,0 +1,13 @@
+package kochnev.kirill.com.exchange;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
